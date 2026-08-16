f137778f04d591f672faa82bc5350e6457256cf5
ndhuytastic <ndhuy.amazing@gmail.com>
2026-08-16 20:21:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -448,10 +448,10 @@ cuc_so_list = [f"阳遁{i}局" for i in range(1, 10)] + [f"阴遁{i}局" for i i
 st.markdown("<div style='height: 5px;'></div>", unsafe_allow_html=True)
 col_opt1, col_opt2, _ = st.columns([2, 2, 6])
 with col_opt1: 
-    manual_hoagiap = st.selectbox("Hoa Giáp", options=["不"] + hoa_giap_60)
+    manual_hoagiap = st.selectbox("Tùy chọn Hoa Giáp (Giờ)", options=["Không dùng"] + hoa_giap_60)
 with col_opt2: 
-    manual_cucso = st.selectbox("Cục Số", options=["不"] + cuc_so_list)
-# --------------------------------------------------
+    manual_cucso = st.selectbox("Tùy chọn Cục Số", options=["Không dùng"] + cuc_so_list)
+# --------------------------------------------------a
 
 user_dt = datetime.combine(selected_date, datetime.min.time()).replace(hour=selected_hour, minute=selected_minute)
 actual_date = user_dt.date() + timedelta(days=1) if user_dt.hour >= 23 else user_dt.date()
@@ -469,12 +469,12 @@ hoa_giap_hien_tai = can_gio + chi_gio
 wl_ju = calculate_correct_ju(wl_yuan, can_gio, chi_gio, wl_jieqi)
 
 # --- CODE GHI ĐÈ LOGIC NẾU NGƯỜI DÙNG CHỌN NÚT ---
-if manual_hoagiap != "不":
+if manual_hoagiap != "Không dùng":
     can_gio = manual_hoagiap[0]
     chi_gio = manual_hoagiap[1]
     hoa_giap_hien_tai = manual_hoagiap
 
-if manual_cucso != "不":
+if manual_cucso != "Không dùng":
     wl_dun = "阳遁" if "阳" in manual_cucso else "阴遁"
     wl_ju = int(manual_cucso.replace("阳遁", "").replace("阴遁", "").replace("局", ""))
 # ------------------------------------------------
